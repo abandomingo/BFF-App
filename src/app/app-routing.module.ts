cb93d845343ed929f57b902f56bbd3c774c5f14f
abandomingo <abandomingo90@gmail.com>
2024-02-17 09:28:06 -0800

    Basic selector options started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { SelectorComponent } from '../components/selector/selector.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'selector', component: SelectorComponent },
+  { path: 'options', component: SelectorComponent },
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: UrlNotFoundComponent },
 ];
--- src/components/selector/selector.component.ts
+++ src/components/selector/selector.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
+export interface Option {
+  content: string;
+  title: string;
+  desc: string;
+}
+
 @Component({
   selector: 'selector',
   templateUrl: './selector.component.html',
   styleUrl: './selector.component.scss'
 })
+
 export class SelectorComponent {
 
+  options: Option[] = [
+    { content: '', title: "Savings Calculator", desc: 'Savings Calculator - Reveal the future value of your efforts, find out how your money can grow! Use this simple calculator to estimate your interest over time.' },
+    { content: '', title: "Budget Calculator", desc: 'Budget Calculator - Create a balanced budget based off your personal needs! Empower yourself to make informed decisions, ensuring your money aligns with your goals and aspirations ' },
+    { content: '', title: "Budget Maker PDF", desc: 'Budget Maker PDF - Download your desired budget in an easy-to-read format. Fill out your own desired budget or import from the Budget Calculator!' }
+  ];
+
+  constructor() {
+    this.options.forEach((option: Option)=>{
+      option.content = option.title
+    })
+  }
+
+  
+
+  onHover(option: Option) {
+    option.content = option.desc;
+  }
+
+  onMouseOut(option: Option) {
+    option.content = option.title;
+  }
 }
